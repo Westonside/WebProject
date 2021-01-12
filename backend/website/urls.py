2bc9960a32c6ed9514c881bff101680afaffa19b
Westonside <weston.t@comcast.net>
2021-01-12 00:16:28 -0800

    server routes added

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "index"),
+    path("/login", views.login, name = "login"),
+    path("/register", views.registerUser, name = "register"),
+    path("/logout", views.logOut, name = "logout")
     ]
--- backend/website/views.py
+++ backend/website/views.py
@@ -1,8 +1,58 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-
+from django.urls import reverse
 from .models import User
+from django.db import IntegrityError
 
 # Create your views here.
+
 def index(request):
-    return render(request, "website/index.html")
+    if request.user.is_anonymous:
+        return HttpResponseRedirect(reverse("login"))
+
+    return HttpResponse("The user logged")
+
+def login(request):
+    
+    if request.method == "POST":
+        userEmail = request.POST["email"]
+        passWord = request.POST["password"]
+
+        user = authenticate(request, email = userEmail, password = passWord)
+
+        if user is not None:
+            login(request,user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return HttpResponse("invalid user")
+    else:
+        return render(request, "website/login.html")
+
+def logOut(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def registerUser(request):
+    if request.method == "POST":
+        userName = request.POST["firstname"]
+        lastName = request.post["lastname"]
+        userName = request.POST["username"]
+        email = request.POST["email"]
+        passWord = request.POST["password"]
+
+        try:
+            user = User.objects.create_user(userName, email, passWord)
+            user.save()
+            login(request, user)
+            return HttpResponse("username taken")
+        except IntegrityError:
+            return HttpResponseRedirect(reverse("index"))
+    
+    elif request.method == "GET":
+        return render(request, "website/login.html")
+
+
+
+    
