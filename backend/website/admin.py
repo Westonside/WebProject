e22fb29bce3051344ddb33b7c01688c8a65bf6d9
Westonside <weston.t@comcast.net>
2021-01-13 23:24:35 -0800

    added the post and the like models

--- backend/website/admin.py
+++ backend/website/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Post, Likes
 # Register your models here
 
 admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Likes)
