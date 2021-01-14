7eee319ced192108d8ef81f10114bb7ee4cdaac1
Westonside <weston.t@comcast.net>
2021-01-13 22:53:25 -0800

    ajax request is now made for the log in, preventing page refresh if incorrect log in

--- backend/website/static/website/script.js
+++ backend/website/static/website/script.js
@@ -1,3 +1,40 @@
 document.addEventListener('DOMContentLoaded', () =>{
-   
+    document.getElementById('login-form').onsubmit = logInSubmit;
+    
 });
+
+function logInSubmit(){
+    //get the form and then loop through its elements searching for password and text type if they are empty highlight them
+    let form = document.getElementById('login-form');
+    let el = form.elements;
+    let success = false;
+    //add additional regex for checking the input
+
+    let email = document.getElementById('email_field');
+    let password = document.getElementById('password_field');
+    success = email.value && password.value ? true : false;
+    if(!success){
+        email.style.borderColor = !email.value ? 'red' : 'none';
+        password.style.borderColor = !password.value ? 'red' : 'none';
+
+    }
+
+    if(success){
+        fetch('/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value
+            })
+        })
+       .then(response =>{
+           if(response.redirected)
+           {
+               //if the server is redirecting the user then follow the url of the redirect
+               window.location.href = response.url;
+           }
+       })
+    }
+    return false;
+    
+}
